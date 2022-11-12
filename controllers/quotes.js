35bcbc73f4ca6060c3dc7da959208d10f8c21f39
Frazier Odhiambo <ofrazy@gmail.com>
2022-11-12 13:01:29 +0300

    working mvp

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -4,31 +4,50 @@ const individual = require('../apis/individualInsurancePlans');
 
 module.exports = {
     setQuotes: async (req,res)=>{
-        let sharedInsurance = 'yes';
-        const sharedPlan = await shared;
-        const individualPlan = await individual;
-
-        // console.log(sharedPlan.insuranceCompanies)
-        if (sharedInsurance == 'yes'){
-            res.render("quotes.ejs", {Cover: sharedPlan});
-        } else{
-            res.render("quotes.ejs", {Cover: individualPlan});
-        };
-       
+        
+       res.render("quotes.ejs")
     },
     getQuotes: async (req,res)=>{
-        let limit = req.body.inpatientLimit;
-        let sharedInsurance = 'yes';
         const sharedPlan = await shared;
         const individualPlan = await individual;
 
-        
+        let limit = req.body.inpatientLimit;
+        let insurance = req.body.insuranceType;
+        let firstName = req.body.firstName;
+        let age = req.body.ageBracket;
+        let spouse = req.body.spouseInclude;
+        let child = req.body.childInclude;
+        let dental = req.body.dentalInclude;
+        let maternity = req.body.maternityInclude;
+        let optical = req.body.opticalInclude;
+                
         console.log(req.body)
-        // console.log(sharedPlan.insuranceCompanies)
-        if (sharedInsurance == 'yes'){
-            res.render("covers.ejs", {Cover: sharedPlan, limit: limit});
+        
+        if (insurance == 'shared'){
+            res.render("covers.ejs", {
+                Cover: sharedPlan, 
+                limit: limit, 
+                insurance: insurance, 
+                firstName: firstName,
+                age: age,
+                spouse: spouse,
+                child: child,
+                dental: dental,
+                maternity: maternity,
+                optical: optical,
+            });
         } else{
-            res.render("covers.ejs", {Cover: individualPlan});
+            res.render("covers.ejs", {Cover: individualPlan, 
+                limit: limit,
+                insurance: insurance, 
+                firstName: firstName,
+                age: age,
+                spouse: spouse,
+                child: child,
+                dental: dental,
+                maternity: maternity,
+                optical: optical,
+            });
         };
        
     },
