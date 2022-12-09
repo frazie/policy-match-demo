94f1e831c70aea2ce25c64494033867594239e4b
Frazier Odhiambo <ofrazy@gmail.com>
2022-12-10 00:29:06 +0300

    added sending to email feature

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -1,5 +1,6 @@
 const insurance = require('../apis/insurancePlans');
-
+const nodemailer = require('nodemailer');
+const cheerio = require('cheerio')
 
 module.exports = {
     setQuotes: async (req,res)=>{
@@ -7,7 +8,7 @@ module.exports = {
     },
     getQuotes: async (req,res)=>{
         const insurancePlan = await insurance;
-
+        
         let limit = req.body.inpatientLimit;
         let firstName = req.body.firstName;
         let age = req.body.ageBracket;
@@ -18,12 +19,14 @@ module.exports = {
         let optical = req.body.opticalInclude;
         let childNumber = req.body.NumberOfChildren;
         let spouseAge = req.body.spouseAge;
+        let email = req.body.email;
                 
         console.log(req.body)
         
-        res.render("covers.ejs", {
+        res.render("coverEmailTest.ejs", {
             Cover: insurancePlan, 
             limit: limit, 
+            email: email,
             insurance: insurance, 
             firstName: firstName,
             age: age,
@@ -37,5 +40,42 @@ module.exports = {
         });
        
     },
+    sendEmail: async (req, res) => {
+        var html = req.body.html
+
+        const $ = cheerio.load(html)
+
+        const userEmail = $('#userEmail').text()
+        
+        function sendToEmail(){
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.nodeEmail,
+                    pass: process.env.nodePass
+                }
+            })
+
+            const mailOptions = {
+                from: process.env.senderEmail,
+                to: userEmail,
+                subject: 'YOUR INSURANCE QUOTATION FROM POLICYMATCH',
+                html
+            }
+
+            transporter.sendMail(mailOptions, function (error, info){
+                if(error){
+                    console.log(error)
+                } else {
+                    console.log('Email sent: ' + info.response)
+                }
+            })
+        }
+
+        sendToEmail()
+
+        console.log(userEmail)
+
+    }
 
 }
--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
+const quotes = require('../controllers/quotes')
 const quotesController = require('../controllers/quotes')
 
 
 router.get('/', homeController.getIndex)
 router.get('/quotes', quotesController.setQuotes)
 router.post('/available-quotes', quotesController.getQuotes)
+router.post('/send-element', quotesController.sendEmail )
 
 
 
