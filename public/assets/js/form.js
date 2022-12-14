e4ebf535687c32689d4106c9f650c528829c65a2
Frazier Odhiambo <ofrazy@gmail.com>
2022-12-14 21:01:38 +0300

    form functions added and edited

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -2,10 +2,10 @@ var create_acc = document.querySelector(".create_acc");
 var main_pages = document.querySelectorAll(".main");
 var next_page = document.querySelector(".next_page");
 var back_page = document.querySelectorAll(".back_page");
-var submit_page = document.querySelector(".submit_page");
 var testa = document.querySelector(".testa");
 var shown_name = document.querySelector(".shown_name");
 let formnumber=0;
+
 create_acc.onclick=function(){
     if(!validateform()){
         return false;
@@ -35,6 +35,35 @@ back_page.forEach(function(back){
 }
 });
 
+document.querySelector('select[name="dentalInclude"]').addEventListener('change', function() {
+    if(this.value === 'yes'){
+        document.querySelector('#optical').style.display = 'block'
+        document.querySelector('#opticalInclude').value = 'yes'
+    }else{
+        document.querySelector('#optical').style.display = 'none'
+        document.querySelector('#opticalInclude').value = 'no'
+    }
+})
+
+document.querySelector('select[name="spouseInclude"]').addEventListener('change', function() {
+    if(this.value === 'yes'){
+        document.querySelector('#spouse').style.display = 'block'
+        
+    }else{
+        document.querySelector('#spouse').style.display = 'none'
+        
+    }
+})
+
+document.querySelector('select[name="childInclude"]').addEventListener('change', function() {
+    if(this.value === 'yes'){
+        document.querySelector('#childrenInfo').style.display = 'block'
+        
+    }else{
+        document.querySelector('#childrenInfo').style.display = 'none'
+        
+    }
+})
 
 function updateform(){
     main_pages.forEach(function(page){
@@ -59,4 +88,5 @@ function validateform(){
     });
     return validate;
 }
+
 
